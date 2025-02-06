202c1f00d133e627fd07548d750627930140d9cd
DESKTOP-0T2LJST\Oleksiy <tsibuliya@gmail.com>
2025-02-06 21:27:59 +0200

    Добавив ДЗ 9.1 та 9.2

---
+++ l9.1/main.py
@@ -0,0 +1,9 @@
+def popular_words (text, words):
+    dic = {}
+    text = text.lower()
+    for s in words:
+        dic[s] = text.count(s + ' ')      # у вхідному тексті після кожного слова є пробіл, тому до слова яке шукаю добавляю пробіл щоб слово шукалось цілком
+    return dic
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
--- test/main.py
+++ test/main.py
@@ -1,7 +1,1 @@
-def reverse_words(sentence):
-    lst = []
-    for s in sentence.split(' '):
-        lst.append(s[::-1])
-    return ' '.join(lst)
-
-print(reverse_words('Hello world'))
+print(len.__doc__)
