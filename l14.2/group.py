1ec18fac61d3ff98a1a0ce34ee991da503da2240
DESKTOP-0T2LJST\Oleksiy <tsibuliya@gmail.com>
2025-02-27 22:08:52 +0200

    Добавив ДЗ 14.1 та 14.2

---
+++ l14.2/group.py
@@ -0,0 +1,32 @@
+from student import Student
+
+# створення власного вийнятка
+class GroupLimitError(Exception):
+    def __init__(self, message="Група не може містити більше 10 студентів."):
+        self.message = message
+        super().__init__(self.message)
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) > 10:
+            raise GroupLimitError()
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
